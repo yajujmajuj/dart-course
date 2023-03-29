9ecd6133674f1044df8d2a07ae9df4ebcbfeeee2
yajujmajuj <apptweak.asohafiz@gmail.com>
2023-03-29 14:44:01 +0500

    .

---
+++ variables_and_types/bin/variables_and_types.dart
@@ -0,0 +1,5 @@
+import 'package:variables_and_types/variables_and_types.dart' as variables_and_types;
+
+void main(List<String> arguments) {
+  print('Hello world: ${variables_and_types.calculate()}!');
+}
